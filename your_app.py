924d99c020fbce113fddddcb7541990b3ec43991
@val314159 <jmward@gmail.com>
2026-05-04 18:12:20 -0500

    pdate

--- your_app.py
+++ your_app.py
@@ -3,13 +3,14 @@ import os
 from celery import Celery
 
 
-celery = Celery("your_app")
-
 db_url = (
     f"postgresql+psycopg2://"
     f"{os.environ['PGUSER']}@{os.environ['PGHOST']}:"
     f"{os.environ['PGPORT']}/{os.environ['PGDATABASE']}"
 )
 
-celery.conf.broker_url = f"sqla+{db_url}"
-celery.conf.result_backend = f"db+{db_url}"
+celery = Celery(
+    "your_app",
+    broker=f"sqla+{db_url}",
+    backend=f"db+{db_url}",
+)
