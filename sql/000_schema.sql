99171b82ab4f93de17f86e80639e519830ce2934
@val314159 <jmward@gmail.com>
2026-05-04 22:14:48 -0500

    firm up schema

--- sql/000_schema.sql
+++ sql/000_schema.sql
@@ -1,28 +1,40 @@
 CREATE TABLE workflows (
     id text PRIMARY KEY,
     display_name text,
+    frozen bool NOT NULL DEFAULT FALSE,
     meta jsonb NOT NULL DEFAULT '{}'::jsonb
 );
 CREATE TABLE tasks (
-    id text PRIMARY KEY,
-    display_name text,
     workflow_id text NOT NULL REFERENCES workflows(id) ON DELETE CASCADE,
-    meta jsonb NOT NULL DEFAULT '{}'::jsonb
+    id text NOT NULL,
+    display_name text,
+    python_class text,
+    task_state text NOT NULL,
+    meta jsonb NOT NULL DEFAULT '{}'::jsonb,
+    PRIMARY KEY (workflow_id, id)
 );
 CREATE TABLE dependencies (
-    -- a child depends on the parent
-    -- this is a dotted arrow from child to parent
-    target_task text NOT NULL REFERENCES tasks(id) ON DELETE CASCADE,
-    depends_on  text NOT NULL REFERENCES tasks(id) ON DELETE CASCADE,
-    PRIMARY KEY (target_task, depends_on)
+    workflow_id text NOT NULL,
+    target_task text NOT NULL,
+    depends_on  text NOT NULL,
+    PRIMARY KEY (workflow_id, target_task, depends_on),
+    FOREIGN KEY (workflow_id, target_task)
+        REFERENCES tasks(workflow_id, id) ON DELETE CASCADE,
+    FOREIGN KEY (workflow_id, depends_on)
+        REFERENCES tasks(workflow_id, id) ON DELETE CASCADE
 );
+CREATE INDEX depends_on_idx ON dependencies(workflow_id, depends_on);
 CREATE TABLE jobs (
     id text PRIMARY KEY,
-    task_id text NOT NULL REFERENCES tasks(id) ON DELETE CASCADE,
-    celery_task_id text NOT NULL,
-    meta jsonb NOT NULL DEFAULT '{}'::jsonb
+    task_workflow_id text NOT NULL,
+    task_id text NOT NULL,
+    celery_task_id text NOT NULL UNIQUE,
+    process_state  text NOT NULL,
+    meta jsonb NOT NULL DEFAULT '{}'::jsonb,
+    created_at  timestamp DEFAULT now() NOT NULL,
+    started_at  timestamp,
+    finished_at timestamp,
+    FOREIGN KEY (task_workflow_id, task_id)
+        REFERENCES tasks(workflow_id, id) ON DELETE CASCADE
 );
-CREATE INDEX tasks_workflow_id_idx ON tasks(workflow_id);
-CREATE INDEX depends_on_idx ON dependencies(depends_on);
-CREATE INDEX jobs_task_id_idx ON jobs(task_id);
-CREATE UNIQUE INDEX jobs_celery_task_id_idx ON jobs(celery_task_id);
+CREATE INDEX jobs_task_id_idx ON jobs(task_workflow_id, task_id);
